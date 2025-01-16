56864912d5c69aed8b6d5038e7820aac221b81e8
Heru <tantoyk7@gmail.com>
2025-01-17 05:15:37 +0700

    feat(dashboard): Create UI Dashboard

---
+++ application/views/pages/dashboard/index.php
@@ -0,0 +1,100 @@
+<div class="page-content">
+
+    <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
+        <div>
+            <h4 class="mb-3 mb-md-0">Welcome to Dashboard, Heru Kristanto</h4>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-xl-12 stretch-card">
+            <div class="row flex-grow-1">
+                <div class="col-md-3 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-header bg-danger"></div>
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <h4 class="card-title mb-0">Program Studi</h4>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-6 col-md-12 col-xl-5">
+                                    <h3 class="mb-2">19</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-header bg-dark"></div>
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <h4 class="card-title mb-0">Dosen</h4>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-6 col-md-12 col-xl-5">
+                                    <h3 class="mb-2">108</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-header bg-primary"></div>
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <h4 class="card-title mb-0">Mahasiswa</h4>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-6 col-md-12 col-xl-5">
+                                    <h3 class="mb-2">10,301</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-header bg-warning"></div>
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <h4 class="card-title mb-0">Mata Kuliah</h4>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-6 col-md-12 col-xl-5">
+                                    <h3 class="mb-2">403</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- row -->
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div id='fullcalendar'></div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- row -->
+    <div id="fullCalModal" class="modal fade">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 id="modalTitle1" class="modal-title"></h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"><span
+                            class="visually-hidden">close</span></button>
+                </div>
+                <div id="modalBody1" class="modal-body"></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- assets/js/fullcalendar.js
+++ assets/js/fullcalendar.js
@@ -5,180 +5,139 @@
 
 (function () {
 
-  const Draggable = FullCalendar.Draggable;
   const calendarEl = document.getElementById('fullcalendar');
   const containerEl = document.getElementById('external-events');
+  const curYear = 2025;
 
-  const curYear = moment().format('YYYY');
-  const curMonth = moment().format('MM');
-
-  // sample calendar events data
-  // Calendar Event Source
   const calendarEvents = {
     id: 1,
-    backgroundColor: 'rgba(1,104,250, .15)',
-    borderColor: '#0168fa',
+    backgroundColor: 'rgb(255, 11, 11)',
+    borderColor: 'rgb(255, 11, 11)',
     events: [
       {
         id: '1',
-        start: curYear+'-'+curMonth+'-08T08:30:00',
-        end: curYear+'-'+curMonth+'-08T13:00:00',
-        title: 'Google Developers Meetup',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
-      },{
+        start: `${curYear}-01-01T00:00:00`,
+        end: `${curYear}-01-01T23:59:59`,
+        title: 'Tahun Baru 2025 Masehi',
+        description: 'Perayaan Tahun Baru Masehi.'
+      },
+      {
         id: '2',
-        start: curYear+'-'+curMonth+'-10T09:00:00',
-        end: curYear+'-'+curMonth+'-10T17:00:00',
-        title: 'Design/Code Review',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
-      },{
+        start: `${curYear}-01-27T00:00:00`,
+        end: `${curYear}-01-27T23:59:59`,
+        title: 'Isra Mikraj Nabi Muhammad SAW',
+        description: 'Peringatan perjalanan spiritual Nabi Muhammad SAW.'
+      },
+      {
         id: '3',
-        start: curYear+'-'+curMonth+'-13T12:00:00',
-        end: curYear+'-'+curMonth+'-13T18:00:00',
-        title: 'Lifestyle Conference',
-        description: 'Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi...'
-      },{
+        start: `${curYear}-01-29T00:00:00`,
+        end: `${curYear}-01-29T23:59:59`,
+        title: 'Tahun Baru Imlek 2576 Kongzili',
+        description: 'Perayaan Tahun Baru Imlek.'
+      },
+      {
         id: '4',
-        start: curYear+'-'+curMonth+'-15T07:30:00',
-        end: curYear+'-'+curMonth+'-15T15:30:00',
-        title: 'Team Weekly Trip',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
-      },{
+        start: `${curYear}-03-29T00:00:00`,
+        end: `${curYear}-03-29T23:59:59`,
+        title: 'Hari Suci Nyepi (Tahun Baru Saka 1947)',
+        description: 'Hari raya umat Hindu untuk introspeksi diri.'
+      },
+      {
         id: '5',
-        start: curYear+'-'+curMonth+'-17T10:00:00',
-        end: curYear+'-'+curMonth+'-19T15:00:00',
-        title: 'DJ Festival',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
-      },{
+        start: `${curYear}-03-31T00:00:00`,
+        end: `${curYear}-03-31T23:59:59`,
+        title: 'Idul Fitri 1446 Hijriah (Hari Pertama)',
+        description: 'Hari raya umat Islam setelah bulan Ramadan.'
+      },
+      {
         id: '6',
-        start: curYear+'-'+curMonth+'-08T13:00:00',
-        end: curYear+'-'+curMonth+'-08T18:30:00',
-        title: 'Carl Henson\'s Wedding',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
-      }
-    ]
-  };
-
-  // Birthday Events Source
-  const birthdayEvents = {
-    id: 2,
-    backgroundColor: 'rgba(16,183,89, .25)',
-    borderColor: '#10b759',
-    events: [
+        start: `${curYear}-04-01T00:00:00`,
+        end: `${curYear}-04-01T23:59:59`,
+        title: 'Idul Fitri 1446 Hijriah (Hari Kedua)',
+        description: 'Hari kedua perayaan Idul Fitri.'
+      },
       {
         id: '7',
-        start: curYear+'-'+curMonth+'-01T18:00:00',
-        end: curYear+'-'+curMonth+'-01T23:30:00',
-        title: 'Jensen Birthday',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
+        start: `${curYear}-04-18T00:00:00`,
+        end: `${curYear}-04-18T23:59:59`,
+        title: 'Wafat Yesus Kristus',
+        description: 'Peringatan wafatnya Yesus Kristus.'
       },
       {
         id: '8',
-        start: curYear+'-'+curMonth+'-21T15:00:00',
-        end: curYear+'-'+curMonth+'-21T21:00:00',
-        title: 'Carl\'s Birthday',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
+        start: `${curYear}-04-20T00:00:00`,
+        end: `${curYear}-04-20T23:59:59`,
+        title: 'Kebangkitan Yesus Kristus (Paskah)',
+        description: 'Perayaan kebangkitan Yesus Kristus.'
       },
       {
         id: '9',
-        start: curYear+'-'+curMonth+'-23T15:00:00',
-        end: curYear+'-'+curMonth+'-23T21:00:00',
-        title: 'Yaretzi\'s Birthday',
-        description: 'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis az pede mollis...'
-      }
-    ]
-  };
-
-
-  const holidayEvents = {
-    id: 3,
-    backgroundColor: 'rgba(241,0,117,.25)',
-    borderColor: '#f10075',
-    events: [
+        start: `${curYear}-05-01T00:00:00`,
+        end: `${curYear}-05-01T23:59:59`,
+        title: 'Hari Buruh Internasional',
+        description: 'Peringatan hari buruh sedunia.'
+      },
       {
         id: '10',
-        start: curYear+'-'+curMonth+'-04',
-        end: curYear+'-'+curMonth+'-06',
-        title: 'Feast Day'
+        start: `${curYear}-05-12T00:00:00`,
+        end: `${curYear}-05-12T23:59:59`,
+        title: 'Hari Raya Waisak 2569 BE',
+        description: 'Peringatan kelahiran, pencerahan, dan wafatnya Buddha.'
       },
       {
         id: '11',
-        start: curYear+'-'+curMonth+'-26',
-        end: curYear+'-'+curMonth+'-27',
-        title: 'Memorial Day'
+        start: `${curYear}-05-29T00:00:00`,
+        end: `${curYear}-05-29T23:59:59`,
+        title: 'Kenaikan Yesus Kristus',
+        description: 'Peringatan kenaikan Yesus Kristus ke surga.'
       },
       {
         id: '12',
-        start: curYear+'-'+curMonth+'-28',
-        end: curYear+'-'+curMonth+'-29',
-        title: 'Veteran\'s Day'
-      }
-    ]
-  };
-
-  const discoveredEvents = {
-    id: 4,
-    backgroundColor: 'rgba(0,204,204,.25)',
-    borderColor: '#00cccc',
-    events: [
+        start: `${curYear}-06-01T00:00:00`,
+        end: `${curYear}-06-01T23:59:59`,
+        title: 'Hari Lahir Pancasila',
+        description: 'Peringatan lahirnya dasar negara Indonesia.'
+      },
       {
         id: '13',
-        start: curYear+'-'+curMonth+'-17T08:00:00',
-        end: curYear+'-'+curMonth+'-18T11:00:00',
-        title: 'Web Design Workshop Seminar'
-      }
-    ]
-  };
-
-  const meetupEvents = {
-    id: 5,
-    backgroundColor: 'rgba(91,71,251,.2)',
-    borderColor: '#5b47fb',
-    events: [
+        start: `${curYear}-06-06T00:00:00`,
+        end: `${curYear}-06-06T23:59:59`,
+        title: 'Idul Adha 1446 Hijriah',
+        description: 'Hari raya kurban bagi umat Islam.'
+      },
       {
         id: '14',
-        start: curYear+'-'+curMonth+'-03',
-        end: curYear+'-'+curMonth+'-05',
-        title: 'UI/UX Meetup Conference'
+        start: `${curYear}-06-27T00:00:00`,
+        end: `${curYear}-06-27T23:59:59`,
+        title: '1 Muharam Tahun Baru Islam 1447 Hijriah',
+        description: 'Peringatan tahun baru dalam kalender Islam.'
       },
       {
         id: '15',
-        start: curYear+'-'+curMonth+'-18',
-        end: curYear+'-'+curMonth+'-20',
-        title: 'Angular Conference Meetup'
-      }
-    ]
-  };
-
-
-  const otherEvents = {
-    id: 6,
-    backgroundColor: 'rgba(253,126,20,.25)',
-    borderColor: '#fd7e14',
-    events: [
+        start: `${curYear}-08-17T00:00:00`,
+        end: `${curYear}-08-17T23:59:59`,
+        title: 'Proklamasi Kemerdekaan Indonesia',
+        description: 'Peringatan Hari Kemerdekaan Republik Indonesia.'
+      },
       {
         id: '16',
-        start: curYear+'-'+curMonth+'-06',
-        end: curYear+'-'+curMonth+'-08',
-        title: 'My Rest Day'
+        start: `${curYear}-09-05T00:00:00`,
+        end: `${curYear}-09-05T23:59:59`,
+        title: 'Maulid Nabi Muhammad SAW',
+        description: 'Peringatan hari kelahiran Nabi Muhammad SAW.'
       },
       {
         id: '17',
-        start: curYear+'-'+curMonth+'-29',
-        end: curYear+'-'+curMonth+'-31',
-        title: 'My Rest Day'
+        start: `${curYear}-12-25T00:00:00`,
+        end: `${curYear}-12-25T23:59:59`,
+        title: 'Kelahiran Yesus Kristus (Hari Natal)',
+        description: 'Perayaan kelahiran Yesus Kristus.'
       }
     ]
   };
 
-  new Draggable(containerEl, {
-    itemSelector: '.fc-event',
-    eventData: function(eventEl) {
-      return {
-        title: eventEl.innerText
-      };
-    }
-  });
+
+
 
 
   // initialize the calendar
@@ -188,13 +147,13 @@
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
     },
-    editable: true,
-    droppable: true, // this allows things to be dropped onto the calendar
+    editable: false,
+    droppable: false, // this allows things to be dropped onto the calendar
     fixedWeekCount: true,
     // height: 300,
     initialView: 'dayGridMonth',
-    timeZone: 'UTC',
-    hiddenDays:[],
+    timeZone: 'UTC+07:00',
+    hiddenDays: [],
     navLinks: 'true',
     // weekNumbers: true,
     // weekNumberFormat: {
@@ -202,23 +161,18 @@
     // },
     dayMaxEvents: 2,
     events: [],
-    eventSources: [calendarEvents, birthdayEvents, holidayEvents, discoveredEvents, meetupEvents, otherEvents],
-    drop: function(info) {
-        // remove the element from the "Draggable Events" list
-        // info.draggedEl.parentNode.removeChild(info.draggedEl);
+    eventSources: [calendarEvents],
+    drop: function (info) {
+      // remove the element from the "Draggable Events" list
+      // info.draggedEl.parentNode.removeChild(info.draggedEl);
     },
-    eventClick: function(info) {
+    eventClick: function (info) {
       console.log(info);
       const eventObj = info.event;
       document.querySelector('#modalTitle1').innerHTML = eventObj.title;
       document.querySelector('#modalBody1').innerHTML = eventObj._def.extendedProps.description || '{ no description }';
       const fullCalModal = new bootstrap.Modal('#fullCalModal');
       fullCalModal.show();
-    },
-    dateClick: function(info) {
-      console.log(info);
-      const createEventModal = new bootstrap.Modal('#createEventModal');
-      createEventModal.show();
     },
   });
 
